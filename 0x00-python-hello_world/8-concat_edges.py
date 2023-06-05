8bde35e0357a74bcec5b1663cee1b817dc44afe3
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-05 14:16:42 -0700

    ensured it is five lines long

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107: 111] + " " + str[:7])
+print(str[39:67] + str[107: 111], end = " ")
+print(str[:7])
