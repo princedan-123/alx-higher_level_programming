64a34ebc5aab6abdb05b1601c3b81dff803323d9
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-06 12:42:08 -0700

    handled negative values

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,10 @@ def print_last_digit(number):
     if number in range(0, 10):
         print(number, end="")
         return number
-    elif number in range(-1, -10, 1):
-        print(number, end="")
-        return number
+    elif number < 0:
+        number = abs(number)
+        print(number % 10, end="")
+        return number % 10
     else:
         number = number % 10
         print(number, end="")
