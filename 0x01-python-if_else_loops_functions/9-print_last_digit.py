969a9f5f56df467124eef72f9574ac5453e26fa7
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-06 09:00:03 -0700

    added proper shebang line

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0 and number <=9:
+    if number in range(0, 10):
+        return number
+    elif number in range(-1, -10):
         return number
     else:
-        return number % 10
+        number = number % 10
+        return number
