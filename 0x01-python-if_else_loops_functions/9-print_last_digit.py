458f03fab136063dc717be95f4f9e29722340ffc
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-06 12:29:15 -0700

    added print statements

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number in range(0, 10):
+        print(number)
         return number
     elif number in range(-1, -10, 1):
+        print(number)
         return number
     else:
         number = number % 10
+        print(number)
         return number
