c6976cca948f2a88288a9c8b82734a9721026142
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-06 12:53:56 -0700

    substracted 32 to get the ascii value of uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strlen = len(str)
+    for i in range(0,strlen):
+        asci = ord(str[i])
+        print("{}".format(chr(asci - 32)), end="")
+    print("\n")
