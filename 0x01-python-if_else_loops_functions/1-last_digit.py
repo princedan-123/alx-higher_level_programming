179e91b270c0d3e46f0c8f355994f5f47124b5c4
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-07 00:53:41 -0700

    used abs to get absolute value of the number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-n = number % 10
+n = abs(number) % 10
 if n == 0:
     print(f"Last digit of {number} is {n} and is 0")
 elif n > 5:
