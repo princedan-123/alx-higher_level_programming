b26ac2e223a3b0397c6ee9b7aa029e5474f3080b
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-07 00:57:02 -0700

    made provision for negative output by multiplying by -1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,5 @@ if n == 0:
 elif n > 5:
     print(f"Last digit of {number} is {n} and is greater than 5")
 elif n < 6 and n != 0:
+    n *= -1
     print(f"Last digit of {number} is {n} and is less than 6 and not 0")
