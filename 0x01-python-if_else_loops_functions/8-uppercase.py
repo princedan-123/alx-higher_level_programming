af88b6484aab42b721b099c5a5e7aa5381e29cf7
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-07 02:30:37 -0700

    made provision for none alphabet characters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     strlen = len(str)
-    for i in range(0,strlen):
-        asci = ord(str[i])
-        print("{}".format(chr(asci - 32),end=''))
-    print("\n")
+    i = 0
+    while i < strlen:
+        char = ord(str[i])
+        if char < 97 or char > 122:
+            print("{}".format(chr(char)), end="" if i < strlen -1 else "\n")
+        elif char >= 97 and char <= 122:
+            char = char - 32
+            print("{}".format(chr(char)), end="" if i < strlen -1 else "\n")
+        i += 1
