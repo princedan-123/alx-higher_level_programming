385f605f3d94011331b038412d20c3ce4c30b2e3
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-08 05:34:31 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    count = len(argv)
+    count -= 1
+    if count == 0:
+        print("{} arguments.".format(count))
+    else:
+        print("{} {}".format(count, "argument" if count == 1 else "arguments"))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
