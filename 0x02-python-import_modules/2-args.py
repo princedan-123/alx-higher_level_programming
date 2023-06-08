a67d29b32def1a5c0aa2606bd73d69972c1eaf58
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-08 08:20:28 -0700

    handles linter error by reducing width of line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     count = len(argv)
     count -= 1
+    arg1 = "argument:"
+    arg2 = "arguments:"
     if count == 0:
         print("{} arguments.".format(count))
     else:
-        print("{} {}".format(count, "argument:" if count == 1 else "arguments:"))
+        print("{} {}".format(count, arg1 if count == 1 else arg2))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
