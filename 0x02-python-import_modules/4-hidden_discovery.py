0081efcb8b6f9e9f2e8308d802fd78f241398682
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-08 07:56:40 -0700

    modified the if statment

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     content = dir(hidden_4)
     count = len(content)
     for i in range(count):
-        if "__" in content:
+        if "__" in content[i]:
             continue
         print(content[i])
