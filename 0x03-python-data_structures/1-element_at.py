bf86b71e7a30a9445a8c61675ee81756677bf484
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-09 07:42:07 -0700

    replaced > with >=

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@ def element_at(my_list, idx):
     count = len(my_list)
     if idx < 0:
         return None
-    elif idx > count:
+    elif idx >= count:
         return None
     else:
         return my_list[idx]
