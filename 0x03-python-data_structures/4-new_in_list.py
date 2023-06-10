b8448584cb7dc0a8f2fbe5c8208441d1280ffcc7
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-10 12:08:21 -0700

    created a copy that refrences a new list object in memory

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a function that replaces an element in a list at a specific 
+position without modifying the original list
+"""
+def new_in_list(my_list, idx, element):
+    copy = my_list[0:]
+    count = len(my_list)
+    if idx < 0:
+        return copy
+    elif idx >= count:
+        return copy
+    else:
+        copy[idx] = element
+        return copy
