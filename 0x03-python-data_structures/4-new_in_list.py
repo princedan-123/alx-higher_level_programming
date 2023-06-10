1f76cffa78097f371cc319354a17fb8d1736ed27
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-10 12:16:32 -0700

    fixed linter error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Write a function that replaces an element in a list at a specific 
+Write a function that replaces an element in a list at a specific
 position without modifying the original list
 """
+
+
 def new_in_list(my_list, idx, element):
     copy = my_list[0:]
     count = len(my_list)
