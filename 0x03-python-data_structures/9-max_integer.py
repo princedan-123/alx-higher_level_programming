d21b40b4ba55d23e0261d84b31fa12680a018f21
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-10 13:38:47 -0700

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Write a function that finds the biggest integer of a list.
+def max_integer(my_list=[]):
+    count = len(my_list)
+    if count == 0:
+        return None
+    else:
+        my_list.sort()
+        return my_list[-1]
