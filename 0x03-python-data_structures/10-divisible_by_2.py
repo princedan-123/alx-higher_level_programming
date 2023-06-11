141a0a616fc8d86cf8cfb84f17d66d93fe8af2b4
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-11 08:46:49 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,11 +2,11 @@
 # Write a function that finds all multiples of 2 in a list.
 def divisible_by_2(my_list=[]):
     count = len(my_list)
-    replica = []
+    replica = my_list[:]
     for i in range(0, count):
         if my_list[i] % 2 == 0:
-            replica.append(True)
+            replica[i] = True
             return replica
         elif my_list[i] % 2 != 0:
-            replica.appenad(False)
+            replica[i] = False
             return replica
