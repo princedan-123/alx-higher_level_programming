8180d6cf64e8ba3293379a9a2617a18f6aeb7ad9
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-11 10:53:49 -0700

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Write a function that adds 2 tuples.
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = list(tuple_a)
+    second = list(tuple_b)
+    count1 = len(first)
+    count2 = len(second)
+    if count1 == 0:
+        first[0, 0]
+    elif count1 == 1:
+        first.append(0)
+    if count2 == 0:
+        second = [0, 0]
+    elif count2 == 1:
+        second.append(0)
+    a = first[0] +  second[0]
+    b = first[1] + second[1]
+    result = (a, b)
+    return result
