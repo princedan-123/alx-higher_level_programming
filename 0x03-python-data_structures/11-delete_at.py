3023cfcdd23767b39ea69a06ab2dccd553d7a065
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-11 09:12:30 -0700

    task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Write a function that deletes the item at a specific position in a list
+def delete_at(my_list=[], idx=0):
+    count = len(my_list)
+    if idx >= count:
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
