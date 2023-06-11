12a637a1a926e17af540e671ac726a1e0685ab8a
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-11 09:51:55 -0700

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 
+Write a function that returns a tuple with the length of
+a string and its first character
+"""
+
+
+def multiple_returns(sentence):
+    count = len(sentence)
+    char = sentence[0]
+    tup = (count, char)
+    return tup
