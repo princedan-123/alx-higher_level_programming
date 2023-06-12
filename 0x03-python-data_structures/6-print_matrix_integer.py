0556b12a8cc1df0e902bd6938d10489e2201e232
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-12 02:53:12 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Write a function that prints a matrix of integers.
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for members in row:
+            if row.index(members) == len(row) - 1:
+                print(members, end="")
+            else:
+                print("{:d}".format(members), end= " ")
+        print()
