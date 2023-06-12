66d075ba0c12ed7505a1bb00039578658bdb3720
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-12 03:34:49 -0700

    task ammended if statement

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 # Write a function that deletes the item at a specific position in a list
 def delete_at(my_list=[], idx=0):
     count = len(my_list)
-    if idx >= count:
+    if idx >= count or idx < 0:
         return my_list
     else:
         del my_list[idx]
