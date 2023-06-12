6bffec6c60d2aa6163a53d1f04bb83cacd007f61
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-12 03:07:30 -0700

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 # Write a function that prints all integers of a list, in reverse order.
 def print_reversed_list_integer(my_list=[]):
     new_list = my_list[::-1]
-    count = len(my_list)
-    for i in range(count):
-        print("{:d}".format(new_list[i]))
+    for i in new_list:
+        print("{:d}".format(i))
