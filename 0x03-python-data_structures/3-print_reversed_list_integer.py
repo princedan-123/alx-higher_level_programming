4c9ae0d6e49511de777d363c45b12daaa6ecaba2
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-13 02:15:23 -0700

    treated the edge case for an empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # Write a function that prints all integers of a list, in reverse order.
 def print_reversed_list_integer(my_list=[]):
+    if my_list == []:
+        print("{}".format(my_list))
     new_list = my_list[::-1]
     for i in new_list:
         print("{:d}".format(i))
