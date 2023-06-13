52c1034b3a2f830ad95cf0e130e3fafc116a798e
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-13 02:51:04 -0700

    treated edge case of empty tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,15 +3,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     count1 = len(tuple_a)
     count2 = len(tuple_b)
+    if count1 == 0:
+        tuple_a = (0, 0)
+    if count2 == 0:
+        tuple_b = (0, 0)
     first = list(tuple_a)
     second = list(tuple_b)
-    if count1 == 0:
-        first[0, 0]
-    elif count1 == 1:
+    if count1 == 1:
         first.append(0)
-    if count2 == 0:
-        second = [0, 0]
-    elif count2 == 1:
+    if count2 == 1:
         second.append(0)
     a = first[0] + second[0]
     b = first[1] + second[1]
