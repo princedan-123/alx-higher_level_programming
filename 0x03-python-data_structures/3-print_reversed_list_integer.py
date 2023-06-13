20e9e91f1a66e66c641b085b4434a00ae7913509
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-13 02:18:38 -0700

    treated the edge case for none list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Write a function that prints all integers of a list, in reverse order.
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
-        print("{}".format(my_list))
+    if my_list == None:
+        return None
     new_list = my_list[::-1]
     for i in new_list:
         print("{:d}".format(i))
