5593339c2805c2f9b2a45f92689e0bf7e90bd56d
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-14 06:01:00 -0700

    fixed linter error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# function that replaces all occurrences of an element by another in a new list.
+# function that replaces all occurrences of an element by another in a new lis
 def search_replace(my_list, search, replace):
     if my_list:
-        check = lambda x: replace if x == search else x
-        new_list = list(map(check, my_list))
+        new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
