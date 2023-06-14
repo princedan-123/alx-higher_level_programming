6b523fceaac77ff9f4506b05dc8f3c151dc0949f
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-14 08:57:37 -0700

    added keys and colon

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# function that prints a dictionary by ordered keys.
+def print_sorted_dictionary(a_dictionary):
+    dic_keys = sorted(list(a_dictionary))
+    for i in dic_keys:
+        print(i, ":", a_dictionary[i])
