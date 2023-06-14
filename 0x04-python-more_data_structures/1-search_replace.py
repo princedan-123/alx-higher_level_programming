3cfa80af314df666d7634e28c670c0d556492f07
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-14 13:09:38 -0700

    removed if statement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # function that replaces all occurrences of an element by another in a new lis
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = list(map(lambda x: replace if x == search else x, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
