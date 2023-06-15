247d4bb9e3d416c8c34ef62960590bd995d273b2
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 00:34:19 -0700

    addressed empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # function that returns a key with the biggest integer value.
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary == {} or a_dictionary is None:
         return None
     values = a_dictionary.values()
     max_value = max(values)
