e27cff07fbae0bc8ab4ccc7d9c0857151830be8f
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-17 05:05:30 -0700

    advanced task 1

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr.bin/python3
+# function that returns the weighted average
+# of all integers tuple (<score>, <weight>)
+def weight_average(my_list=[]):
+    count = len(my_list)
+    if count == 0:
+        return 0
+    sum = 0
+    weighted_sum = 0
+    for rows in my_list:
+        multiply = 1
+        for i in rows:
+            multiply *= i
+        sum += multiply
+    for i in range(0, count):
+        weighted_sum += my_list[i][1]
+    w_average = sum / weighted_sum
+    return w_average
