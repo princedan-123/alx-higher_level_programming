86a7ef67d9ba855732f5dbc1faf59a61f1ad3b23
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 05:42:20 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,12 @@
 # Write a function that divides 2 integers and prints the result.
 def safe_print_division(a, b):
     try:
-        result = a / b
+        answer = None
+        answer = a / b
+        return answer
     except ZeroDivisionError:
         return None
     except Exception:
         return None
-    finally: 
-        print("inside result: {}".format(result))
+    finally:
+        print("inside result: {}".format(answer))
