968ce044e7612ef200b205ab56b210bd58c16f06
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 08:34:48 -0700

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,15 +4,17 @@ def list_division(my_list_1, my_list_2, list_length):
     new = []
     for i in range(0, list_length):
         try:
-            new.append(my_list_1[i] / my_list_2[i])
-        except ValueError:
-            print("wrong type")
-        except TypeError:
+            result = (my_list_1[i] / my_list_2[i])
+        except (ValueError, TypeError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
+            result = 0
         except IndexError:
             print("out of range")
+            result = 0
         except Exception:
-            new.append(0)
+            result = 0
+        finally:
+            new.append(result)
     return new
