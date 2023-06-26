48be44aebbec228a3213f762eff7c117155a7ce3
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 04:14:52 -0700

    task1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Write a function that prints an integer with "{:d}".format().
+def safe_print_integer(value):
+    try:
+        print("{:d}\n".format(value))
+        return True
+    except ValueError:
+        return False
