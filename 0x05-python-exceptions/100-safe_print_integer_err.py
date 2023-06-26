5de5c9e935f9b004a127b0207e9a710b6e6e5c2d
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 09:05:38 -0700

    task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Write a function that prints an integer.
+from sys import stderr as stde
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as e:
+        error = str(e)
+        print("Execption: {}".format(error), file=stde)
+        return False
