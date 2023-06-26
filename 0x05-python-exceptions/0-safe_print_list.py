1024d96f1bcab98b8e07c2169088cc8a564476fe
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 03:53:40 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,11 @@
 # Write a function that prints x elements of a list.
 def safe_print_list(my_list=[], x=0):
     count = 0
-    while count <= x:
+    while count < x:
         try:
-            if count == x:
-                break
             print(my_list[count], end="")
             count += 1
-        except Exception as e:
+        except:
             pass
-    print("\n")
+    print()
     return count
