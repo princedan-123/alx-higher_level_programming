82ee359f21f11cfc813b9140e5cdaabf10e0417d
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 03:56:50 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[count], end="")
             count += 1
-        except:
-            pass
+        except IndexError:
+            break
     print()
     return count
