543bd722171e5d8309651210faf6013863fb207c
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 07:33:24 -0700

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Write a function that divides element by element 2 lists.
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for i in range(0, list_length):
+        try:
+            new.append(my_list_1[i] / my_list_2[i])
+        except ValueError:
+            print("wrong type")
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        except Exception:
+            new.append(0)
