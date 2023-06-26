fd39b9b921381f3f746c82fc7bd0106370778378
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-26 05:44:07 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,7 +7,5 @@ def safe_print_division(a, b):
         return answer
     except ZeroDivisionError:
         return None
-    except Exception:
-        return None
     finally:
         print("inside result: {}".format(answer))
