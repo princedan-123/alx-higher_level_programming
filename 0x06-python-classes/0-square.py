1533bbbfee119bba4af6552bb7a876af1251c58c
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-27 05:20:33 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Write an empty class Square that defines a square
+class Square:
+    """
+    This is an a class called Square.
+    Pass: a keyword that is a placeholder for the body block.
+    """
+    pass
