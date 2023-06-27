afa9c8fc8656a128915b6bbf3ab82fea0df1e704
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-27 05:32:19 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """
-    This is an a class called Square.
+    """This is an a class called Square.
+
     Pass: a keyword that is a placeholder for the body block.
     """
     pass
