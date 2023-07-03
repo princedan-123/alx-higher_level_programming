39212a3ad39ea84cdab6f531a566a1f849841c32
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-03 06:02:14 -0700

    added conditional statement

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,5 +42,7 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
         result = (self.width * 2) + (self.height * 2)
         return result
