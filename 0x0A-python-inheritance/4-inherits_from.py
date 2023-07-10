e0e71c2f373ac7c17d3e3875d8342f58b1906a90
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-10 05:22:47 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""checks if a class of an istance is a subclass of another class"""
+
+
+def inherits_from(obj, a_class):
+    """A function that checks if the class of an object
+    is a subclass of another class
+    arguments:
+        obj(object) whose class is to be inspected
+        a_class(class) the class to be inspected
+    Return: True if the class of the object is a subclass of the specified
+            class else false is returned
+    """
+    istance_class = type(obj)
+    if issubclass(istance_class, a_class) and istance_class is not a_class:
+        return True
+    else:
+        return False
