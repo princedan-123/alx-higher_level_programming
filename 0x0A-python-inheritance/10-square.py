165fed1c23dfa7113b210bc107370e7352da565f
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-10 07:47:49 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,7 +5,7 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
+class Rectangle(Rectangle):
     """ a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
     Instantiation with width and height: def __init__(self, size)
     size  must be private. No getter or setter
