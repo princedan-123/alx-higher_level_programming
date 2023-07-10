4a77cae02cdd4a58d567a5b7f2e7828ddd4dc05d
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-10 08:07:18 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,7 +14,8 @@ class Rectangle(Rectangle):
     def __init__(self, size):
         """instantiates an object"""
         self.integer_validator("size", size)
-        self.__size = width
+        self.__size = size
+        super().__init__("size", size)
 
     def area(self):
         """evaluates the area of the square object"""
