455708bd08fb5ee83abde31300524dc025b5ba25
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-10 04:13:47 -0700

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""checks if an object belongs to a class"""
+
+
+def is_same_class(obj, a_class):
+    """a function that checks for the instance of a class
+    arguments: obj(object): the object to be inspected
+                a_class(class): the class whose instance is to be
+                inspected
+    """
+    if (isinstance(obj, a_class)):
+        return True
+    else:
+        return False
