bf4c635074cbffc8b5223fd89592dc9c3cb1ddca
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-10 07:46:15 -0700

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a class that is a subclass and initialises an object"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Rectangle(BaseGeometry):
+    """ a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    Instantiation with width and height: def __init__(self, size)
+    size  must be private. No getter or setter
+    size  must be positive integers, validated by integer_validator
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = width
+
+    def area(self):
+        return self__size ** 2
