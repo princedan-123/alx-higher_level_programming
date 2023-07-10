182e7175c3ad9b39b7604d8b75d741087ad91f57
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-10 03:42:25 -0700

    taask 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that returns the list of available attributes
+and methods of an object
+"""
+
+def lookup(obj):
+    """ this function returns the attribute of an object
+    arguments:obj(object)
+    Return:A list of attributes is returned
+    """
+    return dir(obj)
