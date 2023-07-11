32d0a98d6ff18d5acbce473265692203902713d9
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-11 09:25:44 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a script that creates a class"""
+
+
+class Student:
+    """a class called student that has the following atttributes
+        Attributes:
+            first_name
+            last_name
+            age
+            _json: retrieves a dictionary of the instance attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, lis=[]):
+        if isinstance(lis, list):
+            if len(lis) == 0:
+                return self.__dict__
+            dic = self.__dict__
+            new = {}
+            for i in lis:
+                if i in dic:
+                    value = dic[i]
+                    new[i] = value
+        return new
