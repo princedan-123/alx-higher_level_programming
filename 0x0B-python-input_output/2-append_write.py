2773b5bb325f391218627eda82ffcecd0747340c
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-11 04:08:08 -0700

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,5 +9,5 @@ def append_write(filename="", text=""):
             text:(string) the string content to be written into the file
         Return: the number of characters written is returned
     """
-    with open(filename, "a", encoding = "utf-8") as File:
+    with open(filename, "a", encoding="utf-8") as File:
         return File.write(text)
