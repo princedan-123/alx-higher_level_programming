02be7110958613ff1b5791f19a5ec9ccc98d134c
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-11 04:07:06 -0700

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that appends a text file"""
+
+
+def append_write(filename="", text=""):
+    """A function that appends a string into a text file
+        Arguments:
+            filename:(string) the name of the file or its relative path
+            text:(string) the string content to be written into the file
+        Return: the number of characters written is returned
+    """
+    with open(filename, "a", encoding = "utf-8") as File:
+        return File.write(text)
