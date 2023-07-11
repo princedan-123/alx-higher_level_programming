e7c28b87e455f7ef9327b11602cd6320ae64ef0c
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-11 03:25:46 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a script that reads a text file (UTF8) and prints it to stdout:"""
+
+
+def read_file(filename=""):
+    """A function that reads and prints the content of a file to STDOUT
+        Arguments
+        filename:(string) the name of the file or its relative path
+    """
+    with open(filename, "r") as f:
+        content = f.read()
+        print(content)
