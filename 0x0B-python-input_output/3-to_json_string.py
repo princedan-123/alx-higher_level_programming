dbb4b6e161cc39e22c82bb70fd55b1f954d3de52
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-11 04:17:26 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that returns the JSON representation of an object (string):"""
+
+
+import json
+def to_json_string(my_obj):
+    """A function that serialises an object
+        arguments:
+        my_obj: the object to be serialised
+        Return: A JSON string is returned
+    """
+    json_string = json.dumps(my_obj)
+    return json_string
