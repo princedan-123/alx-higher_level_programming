5f25251307465acd05ca948c69eb287b08150bc3
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-11 06:28:41 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """this script takes command line argument and saves it to a file"""
 import json
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-from sys import argv
 
 
 with open("add_item.json", "w") as f:
