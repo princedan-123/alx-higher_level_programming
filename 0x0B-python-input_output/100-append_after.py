052dee44fc737c1fccd41f264861d0c2b38c73f4
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-12 02:27:58 -0700

    advanced task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that appends a string to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """A function that appends a string to a file after the occurence
+        of a particular string
+        parameters:
+            filename: the name of the file
+            search_string: the string after which appending occurs
+            new_string: the new string to be appended
+    """
+    lenght = len(search_string)
+    with open(filename, "a+") as f:
+        while f:
+            content = f.read(lenght)
+            if content == "":
+                break
+            elif content == search_string:
+                f.write(new_string)
