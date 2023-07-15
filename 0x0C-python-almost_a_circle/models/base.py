f2bf25de1ebc0eef4759589d7ab7bae94f0397a9
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-07-15 16:10:05 +0000

    file upload

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A script that creates a class"""
+import json
 
 
 class Base:
@@ -20,3 +21,39 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries or len(list_dictionaries) == 0:
+            return []
+        else:
+            json_string = json.dumps(list_dictionaries)
+            return json_string
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        classname = cls.__name__
+        extension = ".json"
+        filename = classname + extension
+        if not list_objs or len(list_objs) == 0:
+            list_objs = []
+        content = cls.to_json_string([i.to_dictionary() for i in list_objs])
+        with open(filename, "w") as f:
+            f.write(content)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            content = list(json.loads(json_string))
+            return content
+
+    @classmethod
+    def create(cls, **dictionary):
+        class_name = cls.__name__
+        if class_name == "Rectangle":
+            obj = cls(id=0, width=1, height=1, x=0, y=0)
+        if class_name == "Square":
+            obj = cls(id=0, size=0, x=0, y=0)
+        return obj.update(**dictionary)
