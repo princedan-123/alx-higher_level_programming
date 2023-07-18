60e60b332e09ae8783bd8e43e32e5b51155af8bc
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-07-18 11:16:53 +0000

    test case

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,6 +5,22 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
+    @staticmethod
+    def _display(width=1, height=1, x=0, y=0):
+        """a static method that will be used to test
+            display method
+        """
+        if x == 0 and y == 0:
+            for i in range(height):
+                print("#" * width)
+        elif x > 0 or y > 0:
+            space = " " * x
+            breadth = "#" * width
+            for i in range(y):
+                print()
+            for i in range(height):
+                print(space + breadth)
+
     def test_rectangle_instatiation(self):
         """testing if the init method, getters and setters works"""
         obj = Rectangle(2, 3, 4, 5, 7)
@@ -50,9 +66,13 @@ class TestRectangle(unittest.TestCase):
             obj3 = Rectangle(1, 2, 3, "4")
         with self.assertRaises(ValueError):
             r5 = Rectangle(0, 0, 3)
+        with self.assertRaises(ValueError):
             obj = Rectangle(1, 0)
+        with self.assertRaises(ValueError):
             r6 = Rectangle(-1, 3, 5)
+        with self.assertRaises(ValueError):
             r7 = Rectangle(2, 4, -1)
+        with self.assertRaises(ValueError):
             r8 = Rectangle(6, 7, 8, -2)
 
     def test_for_int(self):
@@ -188,3 +208,9 @@ class TestRectangle(unittest.TestCase):
         """testing for __str__"""
         r = Rectangle(1, 2, 3, 4, 5)
         self.assertEqual(r.__str__(), "[Rectangle] (5) 3/4 - 1/2")
+
+    def test_display(self):
+        """testing the display method"""
+        result = TestRectangle._display(width=5, height=3)
+        obj = Rectangle(5, 3)
+        self.assertEqual(obj.display(), result)
