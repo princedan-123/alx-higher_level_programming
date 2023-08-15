6de5d6e2b344ef88ab42b8c7277cf78e12678c4a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-15 14:26:53 +0000

    created my first mysql table, fixed typo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates a table in mysql database
 -- CREATE query is used in the creation of the table
-CREATE TABLE IF NOT EXITS first_table (id INT, name VARCHAR(256);
+CREATE TABLE IF NOT EXITS first_table (id INT, name VARCHAR(256));
