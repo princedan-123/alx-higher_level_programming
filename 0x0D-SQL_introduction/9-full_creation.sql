1f774a575cd334e13443a5030f4bd11a802ff170
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-15 15:53:46 +0000

    task 9, created and inserted into a table, fixed typo

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,10 +8,10 @@ INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10)
 -- inserts another row of data
 INSERT INTO second_table (id, name, score)
-VALUES (2, 'Alex' 3)
+VALUES (2, 'Alex', 3)
 -- inserts another row of data
 INSERT INTO second_table (id, name, score)
-VALUES (3, 'Bob' 14)
+VALUES (3, 'Bob', 14)
 -- inserts another row of data
 INSERT INTO second_table (id, name, score)
 VALUES (4, 'George', 8)
