b95b49a10cdb7e19ebcd5f66f8685e9ffd8a5611
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-17 12:50:48 +0000

    task 6 created a table with an auto generated primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates a database and a table within the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	)
