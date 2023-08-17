8abcfe0f18d8bcadaf7f87fe800c8c0458c5e345
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-17 13:10:40 +0000

    task 7 created a table within a database with a foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id FOREIGN KEY(state_id) REFERENCES states(id),
-	name VARCHAR(256)
+	name VARCHAR(256) NOT NULL
 	)
