762252cc8094738be1e7406caf721fb44a6c9e29
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-17 10:49:02 +0000

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- creates a new user
-CREATE IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE  USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
