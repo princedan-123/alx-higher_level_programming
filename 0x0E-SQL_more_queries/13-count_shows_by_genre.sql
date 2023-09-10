f9f7a1c9af49a7c335fa2cbfd356a94a83d750ac
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-09-10 12:45:12 +0000

    task 13 modified

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- joins <TV Show genre and Number of shows linked to this genre
-SELECT gen.name AS genre, COUNT(show.show_id) As number_of_shows
+SELECT gen.name AS genre, COUNT(s.show_id) As number_of_shows
 FROM tv_genres gen
-INNER JOIN  tv_shows_genres show
-ON show.genre_id = gen.id
+INNER JOIN  tv_show_genres s
+ON s.genre_id = gen.id
 GROUP BY gen.name
 HAVING
-COUNT(show.show_id) > 0
+COUNT(s.show_id) > 0
 ORDER BY number_of_shows DESC;
