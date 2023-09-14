8c567b3af939071f134a1796190d0ce7886ab85d
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-09-14 14:52:06 +0000

    fixed the first if statement, changed main to __main__

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@
 import sys
 from MySQLdb import connect
 
-if __name__ == "main":
+if __name__ == "__main__":
     arg = sys.argv
     username = arg[1]
     passwd = arg[2]
@@ -13,7 +13,7 @@ if __name__ == "main":
         with connect(
                 host="localhost", user=username,
                 password=passwd, database=db,
-                port="3306"
+                port=3306
                 ) as mysql_db:
             cursor = mysql_db.cursor()
             cursor.execute("SELECT * FROM states ORDER BY id ASC")
