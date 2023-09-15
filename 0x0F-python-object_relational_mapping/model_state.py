7c795d3ac9342cca6dc3f30f93c3d47ea7b4b941
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-09-15 12:18:14 +0000

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,21 +2,17 @@
 """using sqlachemy library to work with database"""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 import sys
 Base = declarative_base()
 arg = sys.argv
 username = sys.argv[1]
 passwd = sys.argv[2]
 database = sys.argv[3]
-engine = create_engine(
-        """mysql://{}:{}@localhost:3306/{}""".
-        format(username, passwd, database)
-        )
 
 
 class State(Base):
     """A class that maps to a states table in the database"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
