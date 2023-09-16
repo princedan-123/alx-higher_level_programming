a112a51da336b1d96e3a83ec4fbdc9370596353a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-09-16 15:43:07 +0000

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that selects all the state in a table"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    arg = sys.argv
+    username = arg[1]
+    passwd = arg[2]
+    database = arg[3]
+    if len(arg) == 4:
+        engine = create_engine(
+                "mysql+mysqldb://{}:{}@localhost:3306/{}".
+                format(username, passwd, database)
+                )
+    Session = sessionmaker(bind=engine)
+    with Session() as session_object:
+        result = session_object.query(State).filter(State.name.like("%a%"))
+        del_state = result.all()
+        if del_state:
+            for state in del_state:
+                session_object.delete(state)
+        session_object.commit()
