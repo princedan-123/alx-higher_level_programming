ffa981ad83a629048b417c231735afd715193665
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-09-30 12:37:56 -0700

    replaced option with head

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Checks for the availaible method allowed on a resource
-curl -X OPTION -sI "$1" | grep -i '^Allow:' | sed 's/Allow: //i'
+curl -X HEAD -sI "$1" | grep -i '^Allow:' | sed 's/Allow: //i'
