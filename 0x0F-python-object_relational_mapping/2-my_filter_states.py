779c59f260c8fbe3ca966b32be723aaa64be3b9d
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 13:11:48 +0000

    added context manager

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
     search = sys.argv[4]
-    connection = MySQLdb.connect(
+    with MySQLdb.connect(
             host="localhost", user=username, passwd=password, db=database
-            )
-    cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
-            .format(search)
-    cursor.execute(query)
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+            ) as connection:
+        cursor = connection.cursor()
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
+                .format(search)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
