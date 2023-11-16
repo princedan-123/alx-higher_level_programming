1085e853f84e132dca466c4d350726b3608f0b85
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 13:13:29 +0000

    added context manager

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
     search = sys.argv[4]
-    connection = MySQLdb.connect(
+    with MySQLdb.connect(
             host="localhost", user=username, passwd=password, db=database
-            )
-    cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
+            ) as connection:
+        cursor = connection.cursor()
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
             .format(search)
-    if ";" not in search:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
+        if ";" not in search:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
