ef4610ef66728fca1e9c60ace643ec46d79f50ae
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 09:32:33 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script searches for an argument in the database."""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    connection = MySQLdb.connect(
+            host="localhost", user=username, passwd=password, db=database
+            )
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
+            .format(search)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
