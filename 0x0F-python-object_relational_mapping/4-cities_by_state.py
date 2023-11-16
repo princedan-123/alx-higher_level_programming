6a9b0c948ed3e8c7e9d2eccdc69fa70cc5a1a73d
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 10:12:40 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that lists all cities in a state from a database."""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    connection = MySQLdb.connect(
+            host="localhost", user=username, passwd=password, db=database
+            )
+    cursor = connection.cursor()
+    cursor.execute(
+            "SELECT states.id, cities.name, states.name FROM states\
+                    JOIN cities ON states.id = cities.state_id"
+            )
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
