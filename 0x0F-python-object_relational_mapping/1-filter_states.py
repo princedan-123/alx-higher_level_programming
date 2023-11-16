a99286938b3a1047cd163f1f67795c139240ab42
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 13:08:42 +0000

    added context manager

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    connection = MySQLdb.connect(
+    with MySQLdb.connect(
             host="localhost", user=username, passwd=password, db=database
-            )
-    cursor = connection.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
-            )
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+            ) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+                "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
+                )
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
