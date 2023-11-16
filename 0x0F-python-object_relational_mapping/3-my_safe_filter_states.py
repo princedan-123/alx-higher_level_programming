42a7c916ef6d137dbdeb6d75b5ecf45947e19977
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 09:44:30 +0000

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Added a condition to prevent SQL injection."""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    connection = MySQLdb.connect(
+            host="localhost", user=username, passwd=password, db=database
+            )
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
+            .format(search)
+    if ";" not in search:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
