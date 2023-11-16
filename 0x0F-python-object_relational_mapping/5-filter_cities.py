9baf9515bb97eb86e130c569668ced572a1918b7
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-16 10:49:50 +0000

    Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all the cities associated with a state in the database."""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    connection = MySQLdb.connect(
+            host="localhost", user=username, passwd=password, db=database
+            )
+    cursor = connection.cursor()
+    query = "SELECT cities.name FROM cities JOIN states\
+            ON cities.state_id = states.id WHERE states.name = '{}'"\
+            .format(state)
+    if ";" not in state:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cities = []
+        for row in result:
+            cities.append(row[0])
+        output = ", ".join(cities)
+        print(output)
