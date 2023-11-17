66c70240c0930d8ccecfbe0c0ad18e77c9696423
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-17 07:49:11 +0000

    fixed linter error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """A script that uses SQLAlchemy ORM to map a table."""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
+
+
 class State(Base):
     """A class that represents the states table"""
     __tablename__ = 'states'
