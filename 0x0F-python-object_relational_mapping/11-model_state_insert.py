a3dba616c1120713b3210809af47a0fee5be2cee
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-17 09:29:01 +0000

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that uses sqlalchemy ORM to add a state to database."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    port = 3306
+    engine = create_engine(
+        f"mysql://{username}:{password}@localhost:\
+            {port}/{database}"
+            )
+    Session_class = sessionmaker(bind=engine)
+    New_state = State(name="Louisiana")
+    with Session_class() as session:
+        session.add(New_state)
+        result = session.query(State).filter_by(name="Louisiana").first()
+        print(result.id)
