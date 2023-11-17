7b10314a5612b79ea954db3e0b806e1c5cd5a425
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-17 12:46:07 +0000

    added documentation to the class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
+    """A class that is mapped to cities table"""
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
