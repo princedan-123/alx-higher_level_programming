ea153bca624402ca0467724b8df316aeade9845e
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-17 10:05:00 +0000

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that uses sqlalchemy ORM to delete states in database."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    port = 3306
+    engine = create_engine(
+        f"mysql://{username}:{password}@localhost:\
+            {port}/{database}"
+            )
+    Session_class = sessionmaker(bind=engine)
+    with Session_class() as session:
+        result = session.query(State).filter(State.name.like("%a%")).all()
+        for record in result:
+            session.delete(record)
+        session.commit()
