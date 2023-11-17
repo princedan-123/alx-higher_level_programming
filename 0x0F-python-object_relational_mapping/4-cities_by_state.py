ed3c8d435d770a905c050ce3734d020d185b0220
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-17 07:40:17 +0000

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""A script that lists all cities in a state from a database."""
-import MySQLdb
+""" A script that lists all states from the database hbtn_0e_0_usa:
+"""
 import sys
+from MySQLdb import connect
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    with MySQLdb.connect(
-            host="localhost", user=username, passwd=password, db=database
-            ) as connection:
-        cursor = connection.cursor()
-        cursor.execute(
-            "SELECT states.id, cities.name, states.name FROM states\
-                    JOIN cities ON states.id = cities.state_id"
+    arg = sys.argv
+    username = arg[1]
+    passwd = arg[2]
+    db = arg[3]          # database name
+    for name in arg:    # input validation to check injection
+        if ";" in name:
+            validate = False
+        else:
+            validate = True
+    if len(arg) == 4 and validate is True:
+        with connect(
+                host="localhost", user=username,
+                password=passwd, database=db,
+                port=3306
+                ) as mysql_db:
+            cursor = mysql_db.cursor()
+            cursor.execute(
+                    """
+                    SELECT cities.id, cities.name, states.name FROM cities
+                    INNER JOIN states ON cities.state_id = states.id
+                    """
                     )
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
+            result = cursor.fetchall()
+            for i in result:
+                print(i)
