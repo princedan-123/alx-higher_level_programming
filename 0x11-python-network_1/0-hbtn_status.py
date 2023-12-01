5fd1f643356ed000953b6f5436ce5e1dc09daa67
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-12-01 13:17:25 +0000

    made sure script is not executed if imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """A script that opens a TCP connection to a url and prints its content"""
 
 from urllib.request import urlopen
-
-with urlopen("https://alx-intranet.hbtn.io/status") as response:
-    response = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(response)))
-    print("\t- content: {}".format(response))
-    print("\t- utf8 content: {}".format(response.decode("utf-8")))
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode("utf-8")))
