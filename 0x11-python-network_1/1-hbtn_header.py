cb66a0fe5316a066bc414938f6c7a0f489ad1d90
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-12-01 13:19:59 +0000

    added conditions to prevent execution if imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,10 @@
 """A script that takes a url, sends a request and prints a header."""
 from urllib.request import urlopen
 import sys
-
-url = sys.argv[1]
-with urlopen(url) as response:
-    headers =  response.headers
-    id_header = headers.get("X-Request-Id")
-    print(id_header)
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+        with urlopen(url) as response:
+            headers =  response.headers
+            id_header = headers.get("X-Request-Id")
+            print(id_header)
