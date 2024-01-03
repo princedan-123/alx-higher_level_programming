8426934096213dbae27837e3ea225ad3505925ca
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-01-03 14:33:48 +0000

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/* a script that uses request module to send a get request */
+
+const request = require('request');
+
+const callBack = (error, response, body) => {
+  console.log(response.statusCode);
+  if (error) {
+    console.log('');
+  }
+};
+
+if (process.argv.length === 3) {
+  const url = process.argv[2];
+  request(url, callBack);
+}
