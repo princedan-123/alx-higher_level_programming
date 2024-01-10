d6790cc0cf85e105ba6fef01773141ad44ea12c2
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-01-10 14:58:07 +0000

    task 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,11 @@
+/* Handling click event with JQuery API */
+
+$(
+  function () {
+    $('#red_header').click(
+      function () {
+        $('#red_header').addClass('red');
+      }
+    );
+  }
+);
